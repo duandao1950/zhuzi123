ac4b5f5cbf65765b591e68565514f851f50ddd60
duandao1950 <duandao1950@163.com>
2013-04-03 17:47:48 +0800

    t

--- database/mysql/DDL.sql
+++ database/mysql/DDL.sql
@@ -1,97 +1,128 @@
-CREATE  TABLE `zhuzi123`.`OPERATORS` (
-  `OPER_ID` BIGINT(20) NOT NULL ,
-  `OPER_NAME` VARCHAR(45) NOT NULL ,
-  `PASSWORD` VARCHAR(45) NOT NULL ,
-  `IS_VALID` INT NULL ,
-  `REGISTER_TIME` DATETIME NOT NULL ,
-  `LOGIN_TIME` DATETIME NULL ,
-  `BELONG_model` VARCHAR(45) NULL ,
-  `MOBILE_PHONE` VARCHAR(45) NULL ,
-  `FIRST_EMAIL` VARCHAR(45) NULL ,
-  `SECOND_EMAIL` VARCHAR(45) NULL ,
-  PRIMARY KEY (`OPER_ID`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+delimiter $$
 
-CREATE  TABLE `zhuzi123`.`role` (
-  `ROLE_ID` INT NOT NULL ,
-  `ROLE_NAME` VARCHAR(45) NOT NULL ,
-  `DESCRIPTION` VARCHAR(45) NULL ,
-  `CREATE_TIME` DATETIME NULL ,
-  `IS_VALID` INT NULL ,
-  `ORDERNUMBER` INT NULL ,
-  PRIMARY KEY (`ROLE_ID`) )
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `style_id` int(11) DEFAULT NULL,
+  `title` varchar(45) DEFAULT NULL,
+  `author_id` int(11) NOT NULL,
+  `content` longtext NOT NULL,
+  `content_ext` longtext,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=utf8$$
 
-ALTER TABLE `zhuzi123`.`role` CHANGE COLUMN `ROLE_ID` `ROLE_ID` VARCHAR(45) NOT NULL  , ADD COLUMN `id` INT(11) NOT NULL  FIRST 
-, DROP PRIMARY KEY 
-, ADD PRIMARY KEY (`id`) ;
+delimiter $$
 
-ALTER TABLE `zhuzi123`.`role` DROP COLUMN `ORDERNUMBER` , DROP COLUMN `IS_VALID` ;
+CREATE TABLE `article_read` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NOT NULL,
+  `read_count` int(11) DEFAULT '0',
+  `like_count` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
 
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+delimiter $$
 
-CREATE  TABLE `zhuzi123`.`OPER_PRIVILEGE` (
-  `PRIVILEGE_ID` INT NOT NULL ,
-  `PRIVILEGE_NAME` VARCHAR(45) NULL ,
-  `DESCRIPTION` VARCHAR(45) NULL ,
-  PRIMARY KEY (`PRIVILEGE_ID`) )
+CREATE TABLE `author` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `period_id` varchar(45) DEFAULT NULL,
+  `author_name` varchar(45) NOT NULL,
+  `name_pying` varchar(45) DEFAULT NULL,
+  `birth` datetime DEFAULT NULL,
+  `death` datetime DEFAULT NULL,
+  `is_valid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8$$
 
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+delimiter $$
 
-CREATE  TABLE `zhuzi123`.`role_privs` (
-  `ROLE_ID` INT NULL ,
-  `PRIVILEGE_ID` INT NULL )
+CREATE TABLE `author_read` (
+  `id` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `read_count` int(11) DEFAULT '0',
+  `like_count` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
 
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+delimiter $$
 
+CREATE TABLE `dic_key` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `table_name` varchar(45) DEFAULT NULL,
+  `key` varchar(45) DEFAULT NULL,
+  `EFF_DATE` varchar(45) DEFAULT NULL,
+  `EXP_DATE` varchar(45) DEFAULT NULL,
+  `ORDERNUMBER` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8$$
 
-CREATE  TABLE `zhuzi123`.`user_role` (
-  `user_id` BIGINT(20) NOT NULL ,
-  `role_id` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`user_id`) )
-  
-ALTER TABLE `zhuzi123`.`user_role` ADD COLUMN `user_role_id` VARCHAR(45) NOT NULL  FIRST 
-, DROP PRIMARY KEY 
-, ADD PRIMARY KEY (`user_role_id`) ;
+delimiter $$
 
-ALTER TABLE `zhuzi123`.`user_role` CHANGE COLUMN `user_role_id` `id` INT(11) NOT NULL  ;
+CREATE TABLE `dic_value` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `table_name` varchar(45) DEFAULT NULL,
+  `value` varchar(45) DEFAULT NULL,
+  `value_ext1` varchar(45) DEFAULT NULL,
+  `value_ext2` varchar(45) DEFAULT NULL,
+  `value_addi` varchar(45) DEFAULT NULL,
+  `language` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8$$
 
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+delimiter $$
 
+CREATE TABLE `permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `permission_id` varchar(45) NOT NULL,
+  `permission_name` varchar(45) DEFAULT NULL,
+  `permission_note` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8$$
 
-CREATE  TABLE `zhuzi123`.`permission` (
-  `permission_id` VARCHAR(45) NOT NULL ,
-  `permission_name` VARCHAR(45) NULL ,
-  `permission_note` VARCHAR(45) NULL ,
-  PRIMARY KEY (`permission_id`) )
+delimiter $$
 
-ALTER TABLE `zhuzi123`.`permission` ADD COLUMN `id` INT(11) NOT NULL  FIRST 
-, DROP PRIMARY KEY 
-, ADD PRIMARY KEY (`id`) ;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ROLE_ID` varchar(45) NOT NULL,
+  `ROLE_NAME` varchar(45) NOT NULL,
+  `DESCRIPTION` varchar(45) DEFAULT NULL,
+  `CREATE_TIME` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8$$
 
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+delimiter $$
 
+CREATE TABLE `role_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` varchar(45) NOT NULL,
+  `permission_id` varchar(45) NOT NULL,
+  `role_permission_note` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8$$
 
-CREATE  TABLE `zhuzi123`.`role_permission` (
-  `role_permission_id` VARCHAR(45) NOT NULL ,
-  `role_id` VARCHAR(45) NULL ,
-  `permission_id` VARCHAR(45) NULL ,
-  `role_permission_note` VARCHAR(45) NULL ,
-  PRIMARY KEY (`role_permission_id`) )
+delimiter $$
 
-ALTER TABLE `zhuzi123`.`role_permission` DROP COLUMN `role_permission_id` , CHANGE COLUMN `role_id` `role_id` VARCHAR(45) NOT NULL  , CHANGE COLUMN `permission_id` `permission_id` VARCHAR(45) NOT NULL  , ADD COLUMN `id` INT(11) NOT NULL AUTO_INCREMENT  FIRST 
-, DROP PRIMARY KEY 
-, ADD PRIMARY KEY (`id`) ;
-ENGINE = InnoDB
+CREATE TABLE `user` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mobile_phone` varchar(45) DEFAULT NULL,
+  `local_ip` varchar(45) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID` (`ID`),
+  KEY `ID_2` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=gb2312$$
 
-DEFAULT CHARACTER SET = utf8;
+delimiter $$
 
-
-ALTER TABLE `zhuzi123`.`user` ADD COLUMN `mobile_phone` VARCHAR(45) NULL  AFTER `email` , ADD COLUMN `create_time` DATETIME NULL  AFTER `mobile_phone` ;
-ALTER TABLE `zhuzi123`.`user` CHANGE COLUMN `username` `username` VARCHAR(50) NOT NULL  , CHANGE COLUMN `password` `password` VARCHAR(50) NOT NULL  ;
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `role_id` varchar(45) NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8$$
 
